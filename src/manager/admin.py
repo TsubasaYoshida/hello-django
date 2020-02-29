2210e473bfc50ee9ffd5fbd20401b815e6ea6ef4
y_tsubasa <tbs_chako@yahoo.co.jp>
2020-02-29 18:55:41 +0900

    サイト管理画面にリソースを追加する

--- src/manager/admin.py
+++ src/manager/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Brand
+from .models import Brand, Product, Sku
 
 admin.site.register(Brand)
+admin.site.register(Product)
+admin.site.register(Sku)
