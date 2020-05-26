59126c7b17978a28116367f15e857341eb82a4fe
y_tsubasa <tbs_chako@yahoo.co.jp>
2020-05-26 23:48:39 +0900

    status_codeとContent-Typeのチェックをやってみる

--- src/manager/management/commands/http_get.py
+++ src/manager/management/commands/http_get.py
@@ -3,6 +3,10 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        url = 'https://example.com/'
-        response = requests.get(url)
-        print(response)
+        url = 'http://weather.livedoor.com/forecast/webservice/json/v1'
+        params = {'city': 130010}
+        response = requests.get(url, params=params)
+
+        # あまりいけてない
+        print(response.status_code == 200)
+        print(response.headers['Content-Type'] == 'application/json; charset=utf-8')
